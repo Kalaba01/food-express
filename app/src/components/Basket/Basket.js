19464d04bbdb43d8ef659e2a2cc0f656f0d945ee
Kalaba <edinkalaba45@gmail.com>
2024-08-21 14:06:25 +0200

    Dodata visejezicnost za Basket komponentu

--- app/src/components/Basket/Basket.js
+++ app/src/components/Basket/Basket.js
@@ -1,9 +1,11 @@
 import React, { useState, useContext } from 'react';
+import { useTranslation } from 'react-i18next';
 import { BasketContext } from '../../BasketContext';
 import { FaShoppingCart } from 'react-icons/fa';
 import './Basket.css';
 
 function Basket() {
+  const { t } = useTranslation('global');
   const [basketVisible, setBasketVisible] = useState(false);
   const { basket, setBasket } = useContext(BasketContext);
 
@@ -22,21 +24,21 @@ function Basket() {
       <FaShoppingCart size={24} onClick={toggleBasket} className="basket-icon" />
       {basketVisible && (
         <div className="basket-popup">
-          <h2>Your Basket</h2>
+          <h2>{t('Basket.yourBasket')}</h2>
           <ul>
             {basket.map(item => (
               <li key={item.id}>
                 <p>{item.name} (x{item.quantity})</p>
-                <p>Price: {item.price * item.quantity} BAM</p>
-                <button onClick={() => removeFromBasket(item.id)}>Remove</button>
+                <p>{t('Basket.price')}: {item.price * item.quantity} BAM</p>
+                <button onClick={() => removeFromBasket(item.id)}>{t('Basket.remove')}</button>
               </li>
             ))}
           </ul>
           <div className="total-price">
-            <h3>Total: {totalPrice} BAM</h3>
+            <h3>{t('Basket.total')}: {totalPrice} BAM</h3>
           </div>
           {basket.length > 0 && (
-            <button className="order-button">Order</button>
+            <button className="order-button">{t('Basket.order')}</button>
           )}
         </div>
       )}
