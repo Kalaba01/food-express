faf9f11234d1902d3f6601ebc3d1a1eb07896edb
Kalaba <edinkalaba45@gmail.com>
2024-08-28 01:05:49 +0200

    Azurirana logout logika za kurire

--- api/crud/status_crud.py
+++ api/crud/status_crud.py
@@ -1,14 +1,15 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from models.models import Courier
 
 async def get_courier_status(db: Session, courier_id: int):
-    courier = db.query(Courier).filter(Courier.id == courier_id).first()
+    courier = db.query(Courier).filter(Courier.user_id == courier_id).first()
     if not courier:
         raise HTTPException(status_code=404, detail="Courier not found")
     return {"status": courier.status}
 
 async def update_courier_status(db: Session, courier_id: int, new_status: str):
-    courier = db.query(Courier).filter(Courier.id == courier_id).first()
+    courier = db.query(Courier).filter(Courier.user_id == courier_id).first()
     if not courier:
         raise HTTPException(status_code=404, detail="Courier not found")
 
--- app/src/components/Logout/Logout.js
+++ app/src/components/Logout/Logout.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
 import { FaSignOutAlt } from 'react-icons/fa';
+import axios from 'axios';
 import './Logout.css';
 
 function Logout() {
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    navigate('/');
+  const handleLogout = async () => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      const userId = decodedToken.id;
+      const userRole = decodedToken.role;
+
+      if (userRole === 'courier') {
+        try {
+          const response = await axios.put(
+            'http://localhost:8000/courier/status',
+            { id: userId, status: 'offline' },
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+              },
+            }
+          );
+
+          console.log("Response from server:", response.data);
+        } catch (error) {
+          console.error('Failed to update courier status:', error.response?.data || error.message);
+        }
+      }
+
+      localStorage.removeItem('token');
+      navigate('/');
+    }
   };
 
   return (
