b6a7a6f50d6b45cf39863619bd587c737d605874
Kalaba <edinkalaba45@gmail.com>
2024-09-14 12:15:11 +0200

    Implementirani templati za slanje mailova

--- api/utils/scheduled_tasks_utils.py
+++ api/utils/scheduled_tasks_utils.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from database.database import SessionLocal
 from models.models import Request
 from utils.email_utils import send_email
+from utils.email_templates_utils import request_denied_email, request_reminder_email
 
 async def deny_requests_and_send_emails():
     db = SessionLocal()
@@ -17,15 +18,7 @@ async def deny_requests_and_send_emails():
             request.status = "denied"
             db.add(request)
             subject = "Your Request Has Been Denied"
-            body = (
-                f"Dear {request.first_name} {request.last_name},\n\n"
-                "After a thorough review of your request, we regret to inform you that it has been denied.\n"
-                "We appreciate your interest in joining us and encourage you to apply again in the future.\n\n"
-                "If you have any questions or need further assistance, please do not hesitate to contact us at foodexpressproject@outlook.com.\n\n"
-                "Best regards,\n"
-                "The Food Express Team\n"
-                "https://www.foodexpress.com"
-            )
+            body = request_denied_email(request.first_name, request.last_name)
             await send_email(request.email, subject, body)
         db.commit()
     except Exception as e:
@@ -40,15 +33,7 @@ async def remind_pending_requests():
         requests = db.query(Request).filter(Request.status == "pending").all()
         for request in requests:
             subject = "Your Request is Still Under Review"
-            body = (
-                f"Dear {request.first_name} {request.last_name},\n\n"
-                "Thank you for your patience. We wanted to let you know that your request is still under review and will be processed as soon as possible.\n\n"
-                "We appreciate your understanding and will get back to you with an update shortly.\n\n"
-                "If you have any questions in the meantime, please feel free to contact us at foodexpressproject@outlook.com.\n\n"
-                "Best regards,\n"
-                "The Food Express Team\n"
-                "https://www.foodexpress.com"
-            )
+            body = request_reminder_email(request.first_name, request.last_name)
             await send_email(request.email, subject, body)
     except Exception as e:
         print(f"Error while sending reminder emails: {e}")
